7349da0d37bcf275efac59d999f2fdeccfaec184
MadCrane <madcrane2015@gmail.com>
2025-11-25 12:46:14 +0200

    Remove Lines game assets and loaders, keep only HelloPixi game

--- apps/oma-peli/src/routes/+layout.svelte
+++ apps/oma-peli/src/routes/+layout.svelte
@@ -1,21 +1,13 @@
 <script lang="ts">
 	import { type Snippet } from 'svelte';
 	import { GlobalStyle } from 'components-ui-html';
-	import { Authenticate, LoaderStakeEngine, LoaderExample, LoadI18n, AndroidLayoutFix } from 'components-shared';
-	import { setContext } from '../game/context';
+	import { Authenticate, LoadI18n, AndroidLayoutFix } from 'components-shared';
 
 	import messagesMap from '../i18n/messagesMap';
 
 	type Props = { children: Snippet };
 
 	const props: Props = $props();
-
-	let showYourLoader = $state(false);
-
-	const loaderUrlStakeEngine = new URL('../../stake-engine-loader.gif', import.meta.url).href;
-	const loaderUrl = new URL('../../loader.gif', import.meta.url).href;
-
-	setContext();
 </script>
 
 <AndroidLayoutFix />
@@ -27,12 +19,4 @@
 		</LoadI18n>
 	</Authenticate>
 </GlobalStyle>
-
-<LoaderStakeEngine src={loaderUrlStakeEngine} oncomplete={() => (showYourLoader = true)} />
-
-{#if showYourLoader}
-	<LoaderExample src={loaderUrl} />
-	<!-- '/loader.gif' is served from static folder of sveltekit -->
-	<!-- File location: apps/oma-peli/static/loader.gif -->
-{/if}
 
