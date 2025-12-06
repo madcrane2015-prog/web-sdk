62c9a113f852831c2e8c5a44292c6c3a406eb25b
madcrane2015-prog <madcrane2015@gmail.com>
2025-12-06 13:49:05 +0000

    Deploying to gh-pages from @ madcrane2015-prog/web-sdk@5a60ba4bf21cb9fb6727a269c05706e749104e19 🚀

---
+++ oma-peli/_app/immutable/chunks/B4g4H87Q.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1ob0k1)==null?void 0:s.base)??"/web-sdk/oma-peli";var e;const t=((e=globalThis.__sveltekit_1ob0k1)==null?void 0:e.assets)??a;export{t as a,a as b};
---
+++ oma-peli/_app/immutable/nodes/2.C9bc8Oy0.js
@@ -0,0 +1,1 @@
+import{_ as m}from"../chunks/DseNG8B4.js";export{m as component};
