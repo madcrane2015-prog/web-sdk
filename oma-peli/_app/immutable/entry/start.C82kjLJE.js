fbaed69e20b11500971edb9a0de732d130fd5404
MadCrane <madcrane2015@gmail.com>
2025-12-23 16:48:02 +0200

    Deploy v1.1.5 with auto-starting music

---
+++ oma-peli/_app/immutable/entry/start.C82kjLJE.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/BdGP0CqR.js";export{o as load_css,r as start};
--- oma-peli/_app/immutable/entry/start.kfsO-nbi.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/nSi9VEhb.js";export{o as load_css,r as start};
